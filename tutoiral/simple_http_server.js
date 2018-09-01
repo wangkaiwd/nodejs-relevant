dabc6db94ee2ff6d9d88cf464756c831351b0384
wangkaiwd <1162486176@qq.com>
2018-09-01 23:04:40 +0800

    解析get请求中的query参数

--- tutoiral/simple_http_server.js
+++ tutoiral/simple_http_server.js
@@ -1,6 +1,7 @@
 // schema://host:port/path?query#hash
 // port: 22 ssh, 80:http, 443:https, 27017:mongodb
 const http = require('http')
+const qs = require('querystring')
 const server = http.createServer()
 server.listen(8808)
 
@@ -8,6 +9,9 @@ server.on('request', (req, res) => {
   // console.log(req.url)
   res.statusCode = 200
   const {url} = req
+  const queryString = url.slice(url.indexOf('?') + 1)
+  const query = qs.parse(queryString)
+  console.log(query)
   let resStr
   if (url === '/hello') {
     resStr = 'hi there'

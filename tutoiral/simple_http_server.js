72c6e695fc541568bb88c586c7451814a211dd85
wangkaiwd <1162486176@qq.com>
2018-09-02 22:36:27 +0800

    在进行post请求时，有很多问题需要解决 1. 为什么只有在请求头设置application/json的时候，才会执行成功 2. 为什么req.getHeader()方法会报错 3. 为什么在处理contentType不为application/json又出问题

--- tutoiral/simple_http_server.js
+++ tutoiral/simple_http_server.js
@@ -5,20 +5,57 @@ const qs = require('querystring')
 const server = http.createServer()
 server.listen(8808)
 
+const users = []
 server.on('request', (req, res) => {
-  // console.log(req.url)
-  res.statusCode = 200
+  // res.statusCode = 200
   const {url} = req
   const queryString = url.slice(url.indexOf('?') + 1)
+  const path = url.slice(0, url.indexOf('?'))
   const query = qs.parse(queryString)
-  console.log(query)
-  let resStr
-  if (url === '/hello') {
-    resStr = 'hi there'
-  } else if (url === '/bye') {
-    resStr = 'see ya next time'
-  } else {
-    resStr = 'I cant understand what are you saying'
+  // let resStr
+  // if (url === '/hello') {
+  //   resStr = 'hi there'
+  // } else if (url === '/bye') {
+  //   resStr = 'see ya next time'
+  // } else {
+  //   resStr = 'I cant understand what are you saying'
+  // }
+  // console.log(url)
+  console.log(path)
+  switch (path) {
+    case '/users':
+      switch (req.method) {
+        case 'GET':
+          res.statusCode = 200
+          res.end(JSON.stringify(users))
+          break
+        case 'POST':
+          const contentType = req.headers['content-type']
+          if (contentType !== 'application/json') {
+            res.statusCode = 400
+            res.end('error')
+          }
+          let requestBodyStr = ''
+          req.on('data', (data) => {
+            // console.log('data', data.toString())
+            requestBodyStr += data.toString()
+          })
+          req.on('end', () => {
+            const user = JSON.parse(requestBodyStr)
+            users.push(user)
+            res.statusCode = 200
+            res.end(JSON.stringify(user))
+          })
+          // const user = {age: parseInt(Math.random() * 1000)}
+          // users.push(user)
+          // res.statusCode = 200
+          // res.end(JSON.stringify(user))
+          break
+      }
+      break
+    default:
+      res.statusCode = 404
+      res.end('NOT_FOUND')
+      break
   }
-  res.end(resStr)
 })

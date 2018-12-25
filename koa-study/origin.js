3cb38ece87f6772a0cc61c3c7d22f18a22d26419
wangkaiwd <1162486176@qq.com>
2018-12-25 11:37:03 +0800

    feat: 搭建http服务

--- koa-study/origin.js
+++ koa-study/origin.js
@@ -1,1 +1,9 @@
 const http = require('http')
+// http.createServer([options][,requestListener])
+const server = http.createServer((req, res) => {
+  console.log(req, res)
+})
+// server.listen:开启http服务监听与连接
+server.listen(3000, () => {
+  console.log('服务器运行在3000端口')
+})

1534ec7abe18f9e24fb2db2ce6fe1433b86912f3
wangkaiwd <1162486176@qq.com>
2018-12-26 23:43:58 +0800

    feat: url注解

--- koa-study/nodeFromZero/http_url.js
+++ koa-study/nodeFromZero/http_url.js
@@ -1,6 +1,6 @@
 // url: 处理与解析url
-const url = require('url')
-const http = require('http')
+const url    = require('url')
+const http   = require('http')
 const server = http.createServer((req, res) => {
   // 这里用浏览器请求的话会执行俩次
   // 因为还请求了'/favicon.ico'
@@ -26,6 +26,12 @@ server.listen(8000, (err) => {
 
 // URL: 给定的独特资源在web上的地址
 
-// url示例：
+// url示例：https://developer.mozilla.org/zh-CN/docs/Learn/Common_questions/What_is_a_URL
 // http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument
 // 协议(protocol)：http://, 它表明了浏览器必须使用何种协议。可能是：https,http,mialto,ftp
+// 域名(domain Name)：www.example.com,表明正在请求哪个web服务器
+// 端口(port): :80。
+// 网络服务资源路径(path to file)： /path/to/myfile.html
+// 提供给网络服务的额外参数(parameters)： ?key1=value1&key2=value2（用&符号分隔的键值对列表）
+// 资源本身的另一部分的锚点： #SomewhereInTheDocument
+

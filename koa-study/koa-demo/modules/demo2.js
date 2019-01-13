d75dcc5dc5ab175baa1a850e111c93116117fd72
wangkaiwd <1162486176@qq.com>
2019-01-13 21:50:13 +0800

    doc: 常用api文档补充

--- koa-study/koa-demo/modules/demo2.js
+++ koa-study/koa-demo/modules/demo2.js
@@ -20,21 +20,21 @@ import { readFile, writeFileSync, writeFile } from 'fs'
 // )
 
 // async await
-async function read() {
-  // ./name.js是相对于当前进程所在的路径(process.cwd()),而不是相对于当前脚本所在路径
-  // console.log(process.cwd()) // process.cwd():返回进程的当前工作目录
-  // const data = await promisify(readFile)('./name.js')
-  const data = await promisify(readFile)(resolve(__dirname, './name.js'))
-  console.log('data', String(data))
-}
-read()
+// async function read() {
+//   // ./name.js是相对于当前进程所在的路径(process.cwd()),而不是相对于当前脚本所在路径
+//   // console.log(process.cwd()) // process.cwd():返回进程的当前工作目录
+//   // const data = await promisify(readFile)('./name.js')
+//   const data = await promisify(readFile)(resolve(__dirname, './name.js'))
+//   console.log('data', String(data))
+// }
+// read()
 
 // 问题：1.写文件的代码导致node服务一直在重启 2. path.resolve还是不清楚有什么作用
 // path.resolve: 接受多个参数，依次表示所要进入的路径，直到将最后一个参数转为绝对路径
 // 拼接规则： 
 //  1. '/': 会被当成根路径来进行拼接
 //  2. 'a': 如果字符串前边没有任何字符，会解析为当前路径:'./'
-// console.log(resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile')) // /temp/subfile
-// console.log(resolve('/foo/bar', './baz')); // /foo/bar/baz
-// console.log(resolve('/foo/bar', '/tmp/file/')); // 'tmp/file'
-// console.log(resolve('wwwroot', 'static_files/png/', '../gif/image.gif')); // 当前目录下的 /wwwroot/static_files/gif/image.gif
+// console.log('路径1', resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile')) // /temp/subfile
+// console.log('路径2', resolve('/foo/bar', './baz')); // /foo/bar/baz
+// console.log('路径3', resolve('/foo/bar', '/tmp/file/')); // 'tmp/file'
+// console.log('路径4', resolve('wwwroot', 'static_files/png/', '../gif/image.gif')); // 当前目录下的 /wwwroot/static_files/gif/image.gif
--- koa-study/node-api.md
+++ koa-study/node-api.md
@@ -20,12 +20,48 @@
 
 ### 常用`api`学习
 #### `fs.readFile`
+异步的读取文件的全部内容
+
+```js
+const fs = require('fs')
+const path = require('path')
+// 注意：这里的路径是相对于当前进程，而不是当前脚本
+// 这里通过path.resolve将路径拼接为绝对路径
+fs.readFile(path.resolve(__dirname,'../../package.json'), (err,data) => {
+  // data:文件的内容
+  if(err) throw err
+  console.log(JSON.parse(data).name) // koa-study
+})
+```
+
 
 ## `path`模块
-
+用来处理文件与目录的路径
 ### 常用`api`学习
+#### `path.resolve`
+将路径或路径片段处理成绝对路径
+
+拼接规则：  
+* '/': 会被当成根路径来拼接
+* 'a': 字符串会认为是当前路径
+```js
+path.resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile'); // 根路径/tmp/subfile
+path.resolve('/foo/bar', './baz') // 根路径/fool/bar/baz
+path.resolve('wwwroot','static_files/png/', '../gif/image.gif') // 当前目录/wwwroot/static_files/gif/image.gif
+```
+示例：  
+![path.resolve](./shotscreen/path.resolve.png)
+
+#### `path.join`
 
 ## `util`模块
 ### 常用`api`学习
 #### `util.promisify`
+
+
+## 全局变量
+### `process`
+#### `process.cwd`
+返回进程的当前工作目录
+
 

27c6fe68d3971b20a5e812dceef15ace03201a30
wangkaiwd <1162486176@qq.com>
2019-01-29 18:17:12 +0800

    feat: 增加测试demo

---
+++ koa-study/nodeFromZero/node-fs/demo3.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wangkai on 2019/1/29
+ * Desc: fs demo
+ */
+// 1. 判断服务器上面有没有 upload 目录，没有就创建这个目录
+// const fs = require('fs');
+// fs.stat('./upload', (err, stats) => {
+//   if (err) throw err;
+//
+// });
+
+// 2. 找出 html 目录下面的所有的目录，然后打印出来

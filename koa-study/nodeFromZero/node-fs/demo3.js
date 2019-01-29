b2b84aa6304bc0e52e27cf6b63d31a03b66cb5e0
wangkaiwd <1162486176@qq.com>
2019-01-29 18:22:21 +0800

    feat: 增加测试1代码，但是有问题

--- koa-study/nodeFromZero/node-fs/demo3.js
+++ koa-study/nodeFromZero/node-fs/demo3.js
@@ -3,10 +3,17 @@
  * Desc: fs demo
  */
 // 1. 判断服务器上面有没有 upload 目录，没有就创建这个目录
-// const fs = require('fs');
-// fs.stat('./upload', (err, stats) => {
-//   if (err) throw err;
-//
-// });
+const fs = require('fs');
+fs.stat('./upload', (err, stats) => {
+  if (err) throw err;
+  if (!stats.isDirectory()) {
+    fs.mkdir('./upload', (err) => {
+      if (err) throw err;
+      console.log('创建upload目录');
+    });
+  } else {
+    console.log('已经存在upload目录');
+  }
+});
 
 // 2. 找出 html 目录下面的所有的目录，然后打印出来

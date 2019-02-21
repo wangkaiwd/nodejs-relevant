9a0bd7d2b374036fde779d59d5900de036e2f98a
wangkaiwd <1162486176@qq.com>
2019-02-21 23:19:11 +0800

    feat: 使用查询操作符没有生效

---
+++ mongondb-study/practice/demo2.js
@@ -0,0 +1,11 @@
+//17.向numbers中插入500条数据
+// var dataSource = []
+// for(var i=1;i<=500;i++){
+//   dataSource.push({num:i})
+// }
+// db.numbers.insert(dataSource)
+//18.查询numbers中num为50的文档
+// db.numbers.find({num:50})
+//19.查询numbers中num大于50的文档
+// db.nubmers.find({num:{$lt:50}})
+//20.查询numbers中num小于30的文档

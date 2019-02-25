782ffce411f6e1c8e25a4756821b466a06729c51
wangkaiwd <1162486176@qq.com>
2019-02-25 18:09:17 +0800

    feat: path相关api补充

--- nodeBase/05 path/demo2.js
+++ nodeBase/05 path/demo2.js
@@ -7,3 +7,12 @@ console.log(path.basename('tmp/demo/js/test'));
 
 // output: test
 path.basename('tmp/demo/js/test.js', '.js');
+
+// .html
+console.log(path.extname('index.html'));
+
+// output: .md
+console.log(path.extname('index.coffee.md'));
+
+// output: ''
+console.log(path.extname('index'));
---
+++ nodeBase/05 path/demo3.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+// output： '/foo/bar/baz'
+console.log(path.join('/foo', 'bar', 'baz/asdf', '..'));

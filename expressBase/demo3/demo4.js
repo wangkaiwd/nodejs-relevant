4ae002adcd97acb71e453fa876a3554147c51b13
wangkaiwd <1162486176@qq.com>
2019-02-27 09:56:15 +0800

    feat: 通过express.static中间件来提供静态服务器

--- expressBase/demo3/demo4.js
+++ expressBase/demo3/demo4.js
@@ -1,16 +1,20 @@
 const express = require('express');
-// const pug = require('pug');
 const path = require('path');
-// const compiledFunction = pug.compileFile(path.resolve(__dirname, './views/template.pug'));
 const app = express();
 const PORT = 3000;
+// 拆分路由
 const user = require('./routes/user');
-app.set('views', path.resolve(__dirname, './views'));
-app.set('view engine', 'pug');
-// app.get('/', (req, res) => {
-//   res.send(compiledFunction({ name: '李莉' }));
-// });
 
+// express.static(root,[options]): Express唯一内置的中间件，它基于`serve-static`,负责在`express`中提供静态资源
+app.use(express.static(path.resolve(__dirname, './views')));
+
+// 使用模板引擎
+// views: 设置放置模板引擎的目录
+app.set('views', path.resolve(__dirname, './views'));
+// view engine: 设置要用到的模板引擎
+app.set('view engine', 'pug');
+
+// 添加路由
 app.use('/user', user);
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);

1ff46f296ccaad4442c72198fba5c344921df395
wangkaiwd <1162486176@qq.com>
2019-03-02 15:52:17 +0800

    feat: 图片上传 1. 单文件上传  2. 多文件上传

--- expressBase/demo3/demo7.js
+++ expressBase/demo3/demo7.js
@@ -5,11 +5,39 @@ const multer = require('multer');
 const app = express();
 const PORT = 3000;
 
+const storage = multer.diskStorage({
+  // 我们需要负责创建文件夹
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, './upload'));
+  },
+  // 用来生成文件夹中的文件名。如果没有设置filename，每个文件将设置为一个随机文件名
+  // 并且是没有扩展名的
+  filename: function (req, file, cb) {
+    cb(null, Date.now().toString().slice(-4) + '_' + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
 app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('form');
+  res.render('form', { uploadStatus: '点击上传头像' });
+});
+// 单文件上传
+// upload.single(fieldname): 接受一个以field命名的文件。这个文件的信息保存在req.file
+// 注意：这里的fieldname必须和页面html中input 的 name属性的定义相同,否则会出现问题
+// app.post('/profile', upload.single('avatar'), (req, res) => {
+//   console.log(req.file);
+//   console.log('上传成功!');
+//   res.send({ ret_code: 0, data: req.file });
+// });
+// 多文件上传
+app.post('/profile', upload.array('avatar', 4), (req, res) => {
+  console.log(req.files);
+  console.log('上传成功!');
+  res.send({ ret_code: 0, data: req.files });
 });
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);

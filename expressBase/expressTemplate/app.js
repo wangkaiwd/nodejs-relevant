93d1da8aa51470116c0ca4afd4872013f659bbe2
wangkaiwd <1162486176@qq.com>
2019-03-04 23:56:00 +0800

    feat: 添加一些引用库的注释

--- expressBase/expressTemplate/app.js
+++ expressBase/expressTemplate/app.js
@@ -1,7 +1,9 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+// 解析http的请求cookie
 const cookieParser = require('cookie-parser');
+// nodejs http请求记录器中间件
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

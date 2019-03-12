1f36a638eb7dcec5a92635b9e2f919b27aa69b6a
wangkaiwd <1162486176@qq.com>
2019-03-12 10:03:24 +0800

    feat: 补充代码分号

--- expressBase/codeSource/01hello world/demo01.js
+++ expressBase/codeSource/01hello world/demo01.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`app is listening on port ${port}`)
+  console.log(`app is listening on port ${port}`);
 })
